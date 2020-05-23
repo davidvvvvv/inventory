938a647c9b9b2d8026c78acc3a6af4258616b640
davidvvvvv <davidvvvvv@gmail.com>
2020-05-23 12:09:50 +0800

    2020_05_23 12:09

--- src/inputForm.js
+++ src/inputForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useMemo } from "react";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import { LoginContext } from "./loginContext";
 import { navigate } from "@reach/router";
@@ -16,22 +16,31 @@ import {
   Checkbox,
   Icon
 } from "semantic-ui-react";
-import { DateInput} from 'semantic-ui-calendar-react';
+import { DateInput } from 'semantic-ui-calendar-react';
 
 const InputForm = () => {
   const today = new Date(new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()));
   const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(today)
   const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(today)
   const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(today)
+  const todayString = `${ye}-${mo}-${da}`;
 
-  const [rentDate, setRentDate] = useState(`${da}-${mo}-${ye}`);
-  const [expectReturnDate, setExpectReturnDate] = useState(`${da}-${mo}-${ye}`);
+  const [rentDate, setRentDate] = useState(todayString);
+  const [expectReturnDate, setExpectReturnDate] = useState(todayString);
   const [nfcMessage, setNfcMessage] = useState("");
-  const onChange_Rent = (event, data) => setRentDate(data.value);
+  const onChange_Rent = (event, data) => {
+    setRentDate(data.value);
+    setExpectReturnDate(data.value);
+  }
   const onChange_Expect = (event, data) => setExpectReturnDate(data.value);
   const [login, setLogin] = useContext(LoginContext);
   const [activeItem, setActiveItem] = useState("");
-  
+
+  const showTag = useMemo(() => {
+    const newSetRentDate = new Date(rentDate).getTime();
+    const todayStringDate = new Date(todayString).getTime();
+    return todayStringDate > newSetRentDate ? "visible" : "hidden";
+  }, [rentDate])
 
   const logoutAllFunction = () => {
     setActiveItem("logout");
@@ -66,9 +75,9 @@ const InputForm = () => {
         <Header.Content>租借登記頁</Header.Content>
       </Header>
 
-
       <Form size="large">
         <Form.Field>
+           <label style={{visibility:showTag}}>*abc</label>
           <label>租借日期</label>
           <DateInput
             name="rentDate"
@@ -77,6 +86,8 @@ const InputForm = () => {
             iconPosition="left"
             onChange={onChange_Rent}
             animation='none'
+            maxDate={todayString}
+            dateFormat="YYYY-MM-DD"
           />
         </Form.Field>
         <Form.Field>
@@ -88,6 +99,8 @@ const InputForm = () => {
             iconPosition="left"
             onChange={onChange_Expect}
             animation='none'
+            minDate={rentDate}
+            dateFormat="YYYY-MM-DD"
           />
         </Form.Field>
       </Form>
