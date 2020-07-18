70784d7acc0a0154c53506588bd88cf2f3d60fbb
davidvvvvv <davidvvvvv@gmail.com>
2020-07-18 21:06:15 +0800

    test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
         <meta charSet="utf-8" />
         <title>Inventy</title>
         <meta httpEquiv="origin-trial" content="An+ugwRMu6TP9WdggmjXZ7A0w79UP+F01TRh8lEgcSkNYHKVdh2JnmfTkzZPyWGNDoKR3QsWru0CR83uZzRWVgcAAABXeyJvcmlnaW4iOiJodHRwczovL215dGVzdGluZy1jOTQ4OS53ZWIuYXBwOjQ0MyIsImZlYXR1cmUiOiJXZWJORkMiLCJleHBpcnkiOjE1OTMzMDYxOTR9" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Helmet>
       <MainPage />
     </LoginContextProvider>
